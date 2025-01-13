47105897e655c781e04f5110b3a6850e8369b593
Matthew Osborn <matt.osborn@aviture.us.com>
2025-01-13 06:40:54 -0600

    Adding support for dynamic genre

--- scripts/genre-add.py
+++ scripts/genre-add.py
@@ -6,31 +6,32 @@ import eyed3
 
 eyed3.log.setLevel("ERROR")
 
-def process_file(file_path):
+def process_file(file_path, new_genre):
     """Process a single file."""
-    # Replace this with your file processing logic
     print(f"Processing {file_path}")
     audiofile = eyed3.load(file_path)
     genre=audiofile.tag.genre
     
     #print existing tag
-    print("Previous genre: {audiofile.tag.genre}")
+    print(f"Previous genre: {audiofile.tag.genre}")
 
     # update genre if necessary
-    if genre is None or genre.name.find("goo_workout") == -1:
+    if genre is None or genre.name.find(f"{new_genre}") == -1:
         if genre is None:
-            genre = ";goo_workout"
+            genre = f";{new_genre}"
         else:
-            genre = audiofile.tag.genre.name + ";goo_workout"
+            genre = audiofile.tag.genre.name + f";{new_genre}"
         audiofile.tag.genre = genre
         audiofile.tag.save()
 
     #print current tag
-    print("Updated genre: {audiofile.tag.genre")
+    print(f"Updated genre: {audiofile.tag.genre}")
 
 
 def main():
     parser = argparse.ArgumentParser(description="Process a list of files.")
+    parser.add_argument('-d', action='store_true', help='Delete genre from files instead of the default add')
+    parser.add_argument('--genre', type=str, help='Genre to act upon')
     parser.add_argument("files", nargs="+", help="List of files to process")
 
     args = parser.parse_args()
@@ -40,7 +41,7 @@ def main():
             print(f"Error: {file_path} is not a file.")
             continue
 
-        process_file(file_path)
+        process_file(file_path, args.genre)
 
 if __name__ == "__main__":
     main()
